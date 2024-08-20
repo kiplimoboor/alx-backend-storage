8b1da73a0293c3df79006374e89514e37d6ab755
kiplimoboor <kiplimoarapboor@gmail.com>
2024-08-20 17:08:25 +0300

    fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
 -- bands ranker
 
-SELECT origin, SUM(fans) as nb_fans from metal_bands
+SELECT origin, SUM(fans) as nb_fans
+FROM metal_bands
 GROUP BY  origin
-ORDER BY nb_fans desc;
+ORDER BY nb_fans DESC;
